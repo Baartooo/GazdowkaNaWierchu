5ff97bdf5e616264360b92221b5e0229ad917e40
Baartooo <bartek.gajos19@gmail.com>
2020-10-05 21:21:57 +0200

    added dropdown naviation

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,58 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import { Navigation } from './navigation/Navigation';
+import { gsap } from 'gsap';
+import { isMobile } from './helpers/navigation.helper';
 
 import LogoWhite from '../../assets/images/logo-white.png';
 import HamburgerMenu from '../../assets/svg/hamburger-menu.svg';
 import './Navbar.scss';
 
 export const Navbar: FC = () => {
+
+  const [isNavigationOpened, setIsNavigationOpened] = useState<boolean>(isMobile ? false : true);
+  const refNavigation = useRef<HTMLDivElement>(null);
+
+  const tl = gsap.timeline();
+
+  const showNavigation = () => {
+
+    tl.to(refNavigation.current, {
+      transformOrigin: 'top',
+      maxHeight: '500px',
+      duration: .3,
+      ease: 'power1.easeIn',
+    });
+
+  };
+
+  const hideNavigation = () => {
+
+    tl.to(refNavigation.current, {
+      transformOrigin: 'top',
+      maxHeight: 0,
+      duration: .1,
+      ease: 'power1.easeOut',
+    });
+
+  };
+
+  const toggleNavigation = () => {
+    if (isNavigationOpened) {
+      setIsNavigationOpened(false);
+    } else {
+      setIsNavigationOpened(true);
+    }
+  };
+
+  useEffect(() => {
+    if (isNavigationOpened) {
+      showNavigation();
+    } else {
+      hideNavigation();
+    }
+
+    return () => tl.kill();
+  }, [isNavigationOpened]);
 
   return (
     <header className={'navbar'}>
@@ -19,8 +66,8 @@ export const Navbar: FC = () => {
         />
       </div>
 
-      <Navigation />
-      <img src={HamburgerMenu}  className={'navbar__toggle-button'}/>
+      <Navigation wrapperRef={refNavigation} hideNavigation={toggleNavigation} />
+      <img src={HamburgerMenu} className={'navbar__toggle-button'} onClick={toggleNavigation} />
 
     </header>
   );
--- src/components/navbar/navigation/Navigation.tsx
+++ src/components/navbar/navigation/Navigation.tsx
@@ -1,34 +1,43 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { Link } from 'gatsby';
 
 import './Navigation.scss';
 
-export const Navigation: FC = (props) => {
+interface INavigation {
+  wrapperRef: any;
+  hideNavigation: () => void;
+}
+
+export const Navigation: FC<INavigation> = (props) => {
+  const {
+    wrapperRef,
+    hideNavigation,
+  } = props;
 
   return (
-    <nav className={'navbar__navigation'} role={'navigation'}>
+    <nav className={'navbar__navigation'} role={'navigation'} ref={wrapperRef}>
       <div className={'navbar__navigation-item'}>
-        <Link to="/" className={'navbar__navigation-link'} activeClassName={'active'}>
+        <Link to="/" className={'navbar__navigation-link'} activeClassName={'active'} onClick={hideNavigation}>
           witamy
         </Link>
       </div>
       <div className={'navbar__navigation-item'}>
-        <Link to="/offer" className={'navbar__navigation-link'} activeClassName={'active'}>
+        <Link to="/offer" className={'navbar__navigation-link'} activeClassName={'active'} onClick={hideNavigation}>
           oferta
         </Link>
       </div>
       <div className={'navbar__navigation-item'}>
-        <Link to="/about" className={'navbar__navigation-link'} activeClassName={'active'}>
+        <Link to="/about" className={'navbar__navigation-link'} activeClassName={'active'} onClick={hideNavigation}>
           o nas
         </Link>
       </div>
       <div className={'navbar__navigation-item'}>
-        <Link to="/gallery" className={'navbar__navigation-link'} activeClassName={'active'}>
+        <Link to="/gallery" className={'navbar__navigation-link'} activeClassName={'active'} onClick={hideNavigation}>
           galeria
         </Link>
       </div>
       <div className={'navbar__navigation-item'}>
-        <Link to="/contact" className={'navbar__navigation-link'} activeClassName={'active'}>
+        <Link to="/contact" className={'navbar__navigation-link'} activeClassName={'active'} onClick={hideNavigation}>
           kontakt
         </Link>
       </div>
