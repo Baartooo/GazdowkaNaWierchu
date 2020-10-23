436ebd83de57b943ad4760da202e331fda72299b
Baartooo <bartek.gajos19@gmail.com>
2020-10-23 17:16:34 +0200

    general look finished

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -96,8 +96,15 @@ const Contact: FC = (props) => {
               </p>
             </div>
           </div>
-        </div>
 
+          <div className={'contact__map'}>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3170.980236521027!2d19.98686081586652!3d49.35114217383817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4715f19e3d7fc98d%3A0x15519f08d2f1b47c!2sGazd%C3%B3wka%20na%20Wierchu!5e1!3m2!1spl!2spl!4v1603465684806!5m2!1spl!2spl"
+              width="100%" height="100%" frameBorder="0" aria-hidden="false"
+              tabIndex="0" />
+          </div>
+
+        </div>
       </div>
     </StripeContainer>
   );
