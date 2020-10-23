5f430df417ac04bfc1e5a5bc41fb3d77a027e4ad
Baartooo <bartek.gajos19@gmail.com>
2020-10-23 17:49:31 +0200

    polish slugs

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,5 +1,5 @@
 export const home: string = '/';
-export const offer: string = '/offer';
-export const about: string = '/about';
-export const gallery: string = '/gallery';
-export const contact: string = '/contact';
+export const offer: string = '/oferta';
+export const about: string = '/o-nas';
+export const gallery: string = '/galeria';
+export const contact: string = '/kontakt';
