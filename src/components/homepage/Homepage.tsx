3610aba6562a1d01aa063ecb0ff4099b81f3014a
Baartooo <bartek.gajos19@gmail.com>
2020-10-24 18:22:57 +0200

    fixed problem with loading homepage photo

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -4,7 +4,6 @@ import House from '../../assets/images/background2.jpg';
 import './Homepage.scss';
 
 const Homepage: FC = (props) => {
-  const [isBackgroundLoaded, setIsBackgroundLoaded] = useState<boolean>(false);
 
   return (
     <section className={'homepage'}>
@@ -12,10 +11,9 @@ const Homepage: FC = (props) => {
         src={House}
         title={'Domek Gazdówka na Wierchu'}
         alt={'Domek Gazdówka na Wierchu'}
-        className={`homepage__background ${isBackgroundLoaded ? 'loaded' : ''}`}
-        onLoad={() => setIsBackgroundLoaded(true)}
+        className={`homepage__background`}
       />
-      <div className={`homepage__overlay ${isBackgroundLoaded ? 'visible' : ''}`}>
+      <div className={`homepage__overlay`}>
         <h1 className={'homepage__welcome'}>
           Gazdówka na Wierchu <br />
           Zapraszamy!
